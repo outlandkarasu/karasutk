94dc603845375b66514a0df8e74571f1be29c980
outlandkarasu <outland.karasu@gmail.com>
2016-03-19 13:17:46 +0900

    add window options.

--- source/karasutk/gui/mains.d
+++ source/karasutk/gui/mains.d
@@ -14,9 +14,34 @@ import karasutk.dbg : dwritefln;
 import karasutk.gui.sdl : enforceSDL;
 
 /**
- *  Run a dg during GUI.
+ *  GUI option structure.
  */
-void doGuiMain(void delegate() dg) {
+struct GuiOptions {
+    string windowTitle = "karasutk.gui";
+    ushort windowHeight = 1024;
+    ushort windowWidth = 768;
+    ushort windowPositionX = 0;
+    ushort windowPositionY = 0;
+    bool windowCenterX = true;
+    bool windowCenterY = true;
+    bool windowShown = true;
+    bool windowHidden = false;
+    bool windowBorderless = false;
+    bool windowResizeable = false;
+    bool windowMinimized = false;
+    bool windowMaximized = false;
+    bool fullScreen = false;
+    bool fullScreenDesktop = false;
+}
+
+/**
+ *  Run a dg during GUI.
+ *
+ *  Params:
+ *      options = GUI options.
+ *      dg = the main function delegate.
+ */
+void doGuiMain(ref const(GuiOptions) options, void delegate() dg) {
     DerelictSDL2.load();
     scope(exit) DerelictSDL2.unload();
 
@@ -32,12 +57,12 @@ void doGuiMain(void delegate() dg) {
 
     // create the main window
     auto window = enforceSDL(SDL_CreateWindow(
-        toStringz("test"),
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        640,
-        480,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN));
+        toStringz(options.windowTitle),
+        options.windowCenterX ? SDL_WINDOWPOS_CENTERED : options.windowPositionX,
+        options.windowCenterY ? SDL_WINDOWPOS_CENTERED : options.windowPositionY,
+        options.windowWidth,
+        options.windowHeight,
+        SDL_WINDOW_OPENGL | options.windowFlags));
     scope(exit) SDL_DestroyWindow(window);
 
     // create the OpenGL context.
@@ -48,6 +73,23 @@ void doGuiMain(void delegate() dg) {
     DerelictGL3.reload();
     dwritefln("OpenGL version: %s", DerelictGL3.loadedVersion);
 
+    dg();
+
     SDL_Delay(10000);
+}
+
+private:
+
+SDL_WindowFlags windowFlags(ref const(GuiOptions) options) @safe pure nothrow @nogc {
+    SDL_WindowFlags flags;
+    if(options.windowShown) flags |= SDL_WINDOW_SHOWN;
+    if(options.windowHidden) flags |= SDL_WINDOW_HIDDEN;
+    if(options.windowBorderless) flags |= SDL_WINDOW_BORDERLESS;
+    if(options.windowResizeable) flags |= SDL_WINDOW_RESIZABLE;
+    if(options.windowMinimized) flags |= SDL_WINDOW_MINIMIZED;
+    if(options.windowMaximized) flags |= SDL_WINDOW_MAXIMIZED;
+    if(options.fullScreen) flags |= SDL_WINDOW_FULLSCREEN;
+    if(options.fullScreenDesktop) flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
+    return flags;
 }
 
