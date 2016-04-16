93233d0bea9737da0ff2c640de0b3a0b0be1f4c6
outlandkarasu <outland.karasu@gmail.com>
2016-04-16 19:52:57 +0900

    add uniform variables.

--- source/karasutk/gui/shader.d
+++ source/karasutk/gui/shader.d
@@ -9,11 +9,13 @@ module karasutk.gui.shader;
 
 import std.stdio : writefln;
 
+import karasutk.gui.camera: Camera;
 import karasutk.gui.gpu : GpuAsset;
 import karasutk.gui.gl : GlException, checkGlError;
 import karasutk.gui.texture : Texture2d, Rgb;
 
 import derelict.opengl3.gl3;
+import gl3n.linalg : mat4;
 
 /// shader sources
 struct ShaderSource {
@@ -21,13 +23,20 @@ struct ShaderSource {
     string fragmentShader;
 }
 
+/// shader parameters
+struct ShaderParameters {
+    Texture2d!Rgb texture;
+    Camera camera;
+    mat4 model;
+}
+
 /// shader placeholder
 interface Shader : GpuAsset {
 
-    alias ParameterBinder = void delegate(Texture2d!Rgb);
+    alias ParametersBinder = void delegate(ShaderParameters);
 
     /// do process during use program.
-    void duringUse(void delegate(ParameterBinder) dg) const;
+    void duringUse(void delegate(ParametersBinder) dg) const;
 }
 
 /// shader factory 
@@ -64,6 +73,11 @@ class SdlShader : Shader {
         releaseFromGpu();
 
         programId_ = compileProgram(source_);
+        texLocation_ = glGetUniformLocation(programId_, "tex");
+        modelLocation_ = glGetUniformLocation(programId_, "M");
+        viewLocation_ = glGetUniformLocation(programId_, "V");
+        projectionLocation_ = glGetUniformLocation(programId_, "P");
+        mvpLocation_ = glGetUniformLocation(programId_, "MVP");
     }
 
     /// release data from GPU.
@@ -71,28 +85,55 @@ class SdlShader : Shader {
         if(programId_) {
             glDeleteProgram(programId_);
             programId_ = 0;
+            texLocation_ = -1;
+            modelLocation_ = -1;
+            viewLocation_ = -1;
+            projectionLocation_ = -1;
+            mvpLocation_ = -1;
         }
     }
 
     /// do process during use program.
-    void duringUse(void delegate(ParameterBinder) dg) const {
+    void duringUse(void delegate(ParametersBinder) dg) const {
         glUseProgram(programId_);
         scope(exit) glUseProgram(0);
-        dg(&bindParameter);
+        dg(&bindParameters);
     }
 
 private:
 
-    void bindParameter(Texture2d!Rgb texture) {
-        auto texLocation = glGetUniformLocation(programId_, "tex");
+    void bindParameters(ShaderParameters params) const {
+        bindTexture(params.texture);
+        bindCamera(params.model, params.camera);
+    }
+
+    void bindTexture(Texture2d!Rgb texture) const {
+        if(!texture) {
+            return;
+        }
+
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture.id);
-        glUniform1i(texLocation, 0);
+        glUniform1i(texLocation_, 0);
         checkGlError();
+    }
+
+    void bindCamera(ref const mat4 model, ref const Camera camera) const {
+        glUniformMatrix4fv(modelLocation_, 1, GL_TRUE, model.value_ptr);
+        glUniformMatrix4fv(viewLocation_, 1, GL_TRUE, camera.view.value_ptr);
+        glUniformMatrix4fv(projectionLocation_, 1, GL_TRUE, camera.projection.value_ptr);
+
+        immutable mvp = camera.projection * camera.view * model;
+        glUniformMatrix4fv(mvpLocation_, 1, GL_TRUE, mvp.value_ptr);
     }
 
     ShaderSource source_;
     GLuint programId_;
+    GLint texLocation_ = -1;
+    GLint modelLocation_ = -1;
+    GLint viewLocation_ = -1;
+    GLint projectionLocation_ = -1;
+    GLint mvpLocation_ = -1;
 }
 
 /**
